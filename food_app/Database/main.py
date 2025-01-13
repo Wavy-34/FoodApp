3c912dc670aab1105e3caf2cac2eca414a09253c
Wavy-34 <sacha.34.pallares@gmail.com>
2025-01-13 15:21:33 +0100

    [INIT]

--- food_app/Database/main.py
+++ food_app/Database/main.py
@@ -3,8 +3,8 @@ import mysql.connector
 # Connect to MySQL
 conn = mysql.connector.connect(
     host="localhost",
-    user="Claire",
-    password="your_password",
+    user="",
+    password="",
     database="your_database"
 )
 
