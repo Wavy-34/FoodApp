01a40ff64e4595bbed4e41959d674805151eacf5
Wavy-34 <sacha.34.pallares@gmail.com>
2025-01-17 16:11:59 +0100

    [CHORE] that's just trash dw

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from food_app.consts import *
-from food_app.create_hashed_password import hash_password
+from food_app.Database.main import shw_databases
+from food_app.consts import DATABASE_PASSWORD
+from random import random
 
+class ConnardVa(Exception):
+    pass
+
+def pile_ou_face() -> bool:
+    n = random()
+    if n > 0.5:
+        print("Pile !")
+        return True
+    elif n < 0.5:
+        print("Face !")
+        return False
+    else:
+        print("WTF C'EST LA TRANCHE")
+        raise ConnardVa("D'ou tu tombe sur la tranche mdrrrrr")
 def main() -> None:
-    password = DATABASE_PASSWORD
-    print(hash_password(password))
+    pile_ou_face()
+        
 
 if __name__ == '__main__':
     main()
