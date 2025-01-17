d8a8dc2237e95af954a9fdceec3da2604536822b
Wavy-34 <sacha.34.pallares@gmail.com>
2025-01-17 16:12:25 +0100

    [CHORE] that's jsut trash dw

--- main.py
+++ main.py
@@ -2,22 +2,8 @@ from food_app.Database.main import shw_databases
 from food_app.consts import DATABASE_PASSWORD
 from random import random
 
-class ConnardVa(Exception):
-    pass
-
-def pile_ou_face() -> bool:
-    n = random()
-    if n > 0.5:
-        print("Pile !")
-        return True
-    elif n < 0.5:
-        print("Face !")
-        return False
-    else:
-        print("WTF C'EST LA TRANCHE")
-        raise ConnardVa("D'ou tu tombe sur la tranche mdrrrrr")
 def main() -> None:
-    pile_ou_face()
+    pass
         
 
 if __name__ == '__main__':
